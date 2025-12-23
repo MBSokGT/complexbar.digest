05cacaab6687ec090f0b3205cbebb9b07704c170
MBSokGT <nkv2pghg5d@privaterelay.appleid.com>
2025-12-23 15:05:26 +0300

    Update Supabase to new instance for likes system

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://voamxlrutauaicdjtxaq.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvYW14bHJ1dGF1YWljZGp0eGFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1MDEwNDEsImV4cCI6MjA4MTA3NzA0MX0.yoNKfXaSXkcF0UdQgXFdE-2A3BazV-FIw7VYSVheWsM';
+const SUPABASE_URL = 'https://gdfwqciireebkhrpwchn.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdkZndxY2lpcmVlYmtocnB3Y2huIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0ODMyMzEsImV4cCI6MjA4MjA1OTIzMX0.h6K2znKvdf-ypjC6Lotg8Cyzbc2yWp_t_Tx3ouIhsSg'
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
